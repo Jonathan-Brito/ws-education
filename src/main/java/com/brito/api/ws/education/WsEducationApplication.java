1b165d60c73c3d1623cb0e557c8d4aa01f5e30ae
Jonathan Brito Costa <devjonathanbrito@gmail.com>
2023-12-18 09:33:10 -0300

    first commit

---
+++ src/main/java/com/brito/api/ws/education/WsEducationApplication.java
@@ -0,0 +1,13 @@
+package com.brito.api.ws.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brito/api/ws/education/WsEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brito.api.ws.education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
